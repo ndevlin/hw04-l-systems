14f45e128ea290a6bd1022dc90b588762f69e971
Nathan Devlin <ndevlin@seas.upenn.edu>
2021-10-23 17:10:59 -0400

    Updated Turtle, enabled debugging

--- src/Turtle.ts
+++ src/Turtle.ts
@@ -4,21 +4,23 @@ import {vec3, vec4} from 'gl-matrix';
 class Turtle
 {
     position: vec3;
-    orientationAxis: vec3;
-    orientationAngle: number;
+    orientation: vec3;
     recursionDepth: number;
 
-    constructor(posIn: vec3, orientationAxisIn: vec3, 
-        orientationAngleIn: number, recursionDepthIn: number) 
+    constructor(posIn: vec3, orientationIn: vec3,  recursionDepthIn: number) 
     {
         this.position = vec3.fromValues(posIn[0], posIn[1], posIn[2]);
 
-        this.orientationAxis = vec3.fromValues(orientationAxisIn[0], 
-                                                orientationAxisIn[1],
-                                                orientationAxisIn[2]);
+        this.orientation = vec3.fromValues(orientationIn[0], 
+                                                orientationIn[1],
+                                                orientationIn[2]);
 
-        this.orientationAngle = orientationAngleIn;
         this.recursionDepth = recursionDepthIn;
+    }
+
+    moveForward(forwardAmount: number) 
+    {
+        vec3.scaleAndAdd(this.position, this.position, this.orientation, forwardAmount);
     }
 }
 
